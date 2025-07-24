7b79ec24fbb420b8448e6fe808d2c35d145e2ce4
Dominic009 <dpg11000@gmail.com>
2025-07-24 16:44:32 +0600

    Latest changes has been merged with main

--- component/farmerProfile/PersonalInfo.tsx
+++ component/farmerProfile/PersonalInfo.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { use, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import PhotoCaptureModal from "../helper/PhotoCaptureModal";
 import ModalGeneral from "../modal/DialogGeneral";
 import Image from "next/image";
