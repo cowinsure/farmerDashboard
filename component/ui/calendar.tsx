3e2aed44b56e3d2fc695d955f621d866f0acc8a7
cowinsure <cowinsure@gmail.com>
2025-09-04 13:40:32 +0600

    fix: dependecy missing issue

--- component/ui/calendar.tsx
+++ component/ui/calendar.tsx
@@ -54,8 +54,10 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
-        IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
+        NextMonthButton:({ ...props }) => <ChevronLeft className="h-4 w-4" />,
+        PreviousMonthButton:({ ...props }) => <ChevronRight className="h-4 w-4" />,
+        // IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
+        // IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
       }}
       {...props}
     />
